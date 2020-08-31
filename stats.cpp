a6b29ef725ecd31a4a9e2e34b45271597154719c
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-08-31 22:47:39 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,50 @@
 #include "stats.h"
+#include <cfloat>
+#include <limits>
+using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+double AverageCalculation (const std::vector<double>& numbers)
+{ 
+    double avg = 0;
+    for(int i=0; i<numbers.size; i++)
+    {
+        avg = avg + numbers[i];
+    }
+    avg = avg/numbers.size();
+    return avg;
+}
+
+double MinCalculation(const std::vector<double>& numbers)
+{ 
+    double min = numbers[0];
+    for(int i=0; i<numbers.size(); i++)
+    {
+        if (numbers[i] < min)
+        { min = values[i];
+        }
+    }
+    return min;
+}
+
+double MaxCalculation(const std::vector<double>& numbers)
+{ 
+    double max = numbers[0]
+    for(int i=0; i<numbers.size(); i++)
+    {
+        if (numbers[i] > max)
+        { max = values[i];
+        }
+    }
+    return max;
+}
+
+Stats Statistics::ComputeStatistics(const std::vector<double>& numbers)
+{
     //Implement statistics here
+    Stats ComputedNumbers;
+    ComputedNumbers.set_avg(AverageCalculation(numbers));
+    ComputedNumbers.set_min(MinCalculation(numbers));
+    ComputedNumbers.set_max(MaxCalculation(numbers));
+    
+    return ComputedNumbers;
 }
