c26f4909559de26e51e3131ae72345eb64966cb6
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-08-31 22:53:36 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -3,22 +3,11 @@
 namespace Statistics {
     class stats
     {
+        public :
         double average;
         double min;
         double max;
-        public :
-        void set_avg(double avg)
-        {
-            this->average = avg;
-        }
-        void set_min (double min)
-        {
-            this->min = min;
-        }
-        void set_max(double max)
-        {
-            this->max = max;
-        }
+            
     };
     
     stats ComputeStatistics(const std::vector<double>& );
