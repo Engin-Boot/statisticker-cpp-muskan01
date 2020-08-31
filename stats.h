8c4fe3569527687556422ebfe097625b154057ac
muskan01 <35635037+muskan01@users.noreply.github.com>
2020-08-31 23:03:01 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -7,7 +7,11 @@ namespace Statistics {
         double average;
         double min;
         double max;
-            
+        stats()
+       { average = 0;
+          min = 0;
+          max = 0;
+       }
     };
     
     stats ComputeStatistics(const std::vector<double>& );
